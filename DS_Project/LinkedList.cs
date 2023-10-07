4cc4c99e9b8bce3794144711e36c7726070adf48
Mithu0229 <showkat131080@gmail.com>
2023-10-07 16:17:54 +0600

    Add project files.

---
+++ DS_Project/LinkedList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Project
+{
+	// Node class
+	public class Node
+	{
+		public int element;
+		public Node next;
+        public Node(int e, Node n)
+        {
+			element = e;
+			next = n;
+        }
+    }
+	public class LinkedList
+	{
+		private Node Head;
+		private Node Tail;
+		private int Size;
+        public LinkedList()
+        {
+			Head = null;
+			Tail = null;
+			Size = 0;
+        }
+
+		public int Lenght()
+		{
+			return Size;
+		}
+		public bool isEmpty()
+		{
+			return Size == 0;
+		}
+
+
+		public void AddLast(int e)
+		{
+			Node newest = new Node(e, null);
+			if (isEmpty())
+			{
+				Head = newest;
+			}
+			else
+			{
+				Tail.next = newest;
+			}
+			Tail = newest;
+			Size = Size + 1;
+		}
+
+		public void AddFirst(int e)
+		{
+			Node newest = new Node(e, null);
+			if (isEmpty()) 
+			{
+				Head = newest;
+				Tail = newest;
+			}
+			else
+			{
+				newest.next = Head;
+				Head = newest;
+
+			}
+			Size = Size + 1;
+		}
+		public void Display()
+		{
+			Node p = Head;
+			while (p != null)
+			{
+				Console.WriteLine(p.element + "----->");
+				p = p.next;
+				Size = Size + 1;
+			}
+		}
+
+    }
+}
---
+++ DS_Project/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+using DS_Project;
+
+LinkedList l = new LinkedList();
+l.AddLast(7);
+l.AddLast(3);
+l.AddLast(4);
+l.AddLast(5);
+l.AddFirst(1);
+l.Display();
+Console.WriteLine("Hello, World!");
